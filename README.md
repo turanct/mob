6470ba48435af3807f16880e8acde18a159dafe9
turanct <spinnewebber_toon@hotmail.com>
2020-03-18 00:18:19 +0100

    Reduced the amount of commands needed to work with this tool
    - mob start was not needed, now it's done automatically when doing mob
      switch.
    - mob commit is not used if mob continue does everything and applies the
      changes to the main branch without using actual commits. It's just as
      if the changes were copied from the other computer.

--- README.md
+++ README.md
@@ -3,16 +3,11 @@
 ## How does it work?
 
 - It takes the current branch you're on as the "main branch"
-- When you use `mob start`, it creates a wip branch for the current
-  main branch you're on. When you want to switch drivers, you just do
-- `mob switch` which creates a wip commit and pushes it to the wip
-  branch. It then switches you back to the main branch.
+- When you want to switch drivers, you just do `mob switch`
+  which creates a wip commit and pushes it to a wip branch.
+  It then takes you back to the main branch.
 - On another computer, you can use `mob continue` to continue where you
   left off with the previous driver.
-- When you're ready to make a commit that's not a wip, you can use
-  `mob commit` which will take the wip commits and apply their changes
-  to the main branch, but not actually commit them. At that point you
-  can create a deliberate commit.
 
 
 ## Installing
@@ -29,10 +24,8 @@ ln -s $(pwd)/mob.sh /usr/local/bin/mob
 
 ## Commands
 
-- `mob start`
 - `mob switch`
 - `mob continue`
-- `mob commit`
 
 
 ## Credit & Why
--- mob.sh
+++ mob.sh
@@ -24,24 +24,17 @@ usage() {
 	Usage: $PROGNAME subcommand
 
 	$PROGNAME takes the current branch you're on as the "main branch".
-	When you use "mob start", it creates a wip branch for the current
-	"main branch" you're on. When you want to switch drivers, you just
-	do "mob switch" which creates a wip commit and pushes it to the wip
-	branch. It then switches you back to the main branch.
+	When you want to switch drivers, you just do "mob switch" which
+	creates a wip commit and pushes it to a wip branch. It then
+	switches you back to the main branch.
 
 	On another computer, you can use "mob continue" to continue where you
-	left off with the previous driver.
-
-	When you're ready to make a commit that's not a wip, you can use
-	"mob commit" which will take the wip commits and apply their changes
-	to the main branch, but not actually commit them. At that point you
-	can create a deliberate commit.
+	left off with the previous driver. It will take the wip commits from
+	the wip branch and apply them on the "main branch".
 
 	SUBCOMMANDS:
-	   start         Prepare the wip-branch based the "main branch you're on".
 	   switch        Commit and push everything that changed on the wip branch.
 	   continue      Continue working on the wip branch after somebody switched.
-	   commit        Prepare a commit to the "main branch" based on the wip commits.
 	EOF
 }
 
@@ -52,14 +45,10 @@ main() {
     fi
 
     case $1 in
-    start)
-        if [ "$CURRENT_BRANCH_IS_WIP" == 0 ]; then
-            git pull --ff-only origin $BRANCH
-            git checkout -b $WIPBRANCH
-        fi
-        ;;
     switch)
-        if [ $CURRENT_BRANCH_IS_WIP == 1 ]; then
+        if [ $CURRENT_BRANCH_IS_WIP == 0 ]; then
+            git pull --ff-only origin $BRANCH
+            git checkout -b $WIPBRANCH || git checkout $WIPBRANCH
             git add .
             git commit -m "wip $WHOAMI" || true
             git push origin $WIPBRANCH
@@ -72,28 +61,16 @@ main() {
             git pull --rebase origin $BRANCH
             git checkout $WIPBRANCH
             git pull --rebase origin $WIPBRANCH
-        fi
-        ;;
-    commit)
-        if [ $CURRENT_BRANCH_IS_WIP == 1 ]; then
-            git add .
-            git commit -m "wip $WHOAMI" || true
 
-            git remote update
-            git pull --rebase origin $WIPBRANCH
-            git checkout $ORIGINALBRANCH
-            git pull --rebase origin $ORIGINALBRANCH
-
-            git checkout $WIPBRANCH
-            git checkout -b "wip-commit-$BRANCH"
             git reset --soft $ORIGINALBRANCH
-
+            git restore --staged .
+            git stash push --include-untracked .
+            git pull origin $WIPBRANCH
             git checkout $ORIGINALBRANCH
-            git branch -d "wip-commit-$BRANCH"
+            git stash pop
+
             git branch -D $WIPBRANCH
             git push origin ":$WIPBRANCH"
-
-            git restore --staged .
         fi
         ;;
     *)
